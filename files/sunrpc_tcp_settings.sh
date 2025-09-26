ff71b0b7eafe54034ccb17ca00648d1017535186
Sergei Petrosian <spetrosi@redhat.com>
2025-09-26 17:19:10 +0200

    Load sunrpc module with modprobe instead of creating a service

--- files/sunrpc_tcp_settings.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eu
-
-# Load the sunrpc module and apply the sysctls configured for the subsystem.
-# This will be run from a startup service to ensure the tunables are always set
-# on boot.
-modprobe sunrpc
-sysctl -p
