816c861797ff0cbe90140b334bde5f863629773f
Rich Megginson <rmeggins@redhat.com>
2025-10-10 14:04:45 -0600

    ci: ostree wip
    Show how to configure package repos for use with ostree
    image building and https://github.com/linux-system-roles/tox-lsr/pull/213
    
    Signed-off-by: Rich Megginson <rmeggins@redhat.com>

--- .ostree/get_ostree_data.sh
+++ .ostree/get_ostree_data.sh
@@ -6,15 +6,16 @@ ostree_dir="${OSTREE_DIR:-"$(dirname "$(realpath "$0")")"}"
 
 if [ -z "${4:-}" ] || [ "${1:-}" = help ] || [ "${1:-}" = -h ]; then
     cat <<EOF
-Usage: $0 packages [runtime|testing] DISTRO-MAJOR[.MINOR] [json|yaml|raw|toml]
-The script will use the packages and roles files in $ostree_dir to
-construct the list of packages needed to build the ostree image.  The script
-will output the list of packages in the given format
+Usage: $0 packages|repos runtime|testing DISTRO-MAJOR[.MINOR] json|yaml|raw|toml
+The script will use the packages, repos and roles files in $ostree_dir to
+construct the list of packages or repos needed to build the ostree image.  The script
+will output the list of packages or repos in the given format
 - json is a JSON list like ["pkg1","pkg2",....,"pkgN"]
 - yaml is the YAML list format
-- raw is the list of packages, one per line
+- raw is the list of packages, one per line, or repos in INI .repo format
 - toml is a list of [[packages]] elements as in https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/9/html-single/composing_installing_and_managing_rhel_for_edge_images/index#creating-an-image-builder-blueprint-for-a-rhel-for-edge-image-using-the-command-line-interface_composing-a-rhel-for-edge-image-using-image-builder-command-line
 The DISTRO-MAJOR.MINOR is the same format used by Ansible for distribution e.g. CentOS-8, RedHat-8.9, etc.
+- toml is the repo format needed for osbuild-composer
 EOF
     exit 1
 fi
@@ -68,7 +69,7 @@ get_packages() {
     local ostree_dir pkgtype pkgfile rolefile
     ostree_dir="$1"
     for pkgtype in "${pkgtypes[@]}"; do
-        for suff in "" "-$distro" "-${distro}-${major_ver}" "-${distro}-${ver}"; do
+        for suff in "${suffix_list[@]}"; do
             pkgfile="$ostree_dir/packages-${pkgtype}${suff}.txt"
             if [ -f "$pkgfile" ]; then
                 cat "$pkgfile"
@@ -120,10 +121,81 @@ format_packages_toml() {
     done
 }
 
+get_repos() {
+    local ostree_dir repotype repofile rolefile
+    ostree_dir="$1"
+    for repotype in "${pkgtypes[@]}"; do
+        for suff in "${suffix_list[@]}"; do
+            repofile="$ostree_dir/repos-${repotype}${suff}.repo"
+            if [ -f "$repofile" ]; then
+                cat "$repofile"
+            fi
+        done
+        rolefile="$ostree_dir/roles-${repotype}.txt"
+        if [ -f "$rolefile" ]; then
+            local roles role rolepath
+            roles="$(cat "$rolefile")"
+            for role in $roles; do
+                rolepath="$(get_rolepath "$ostree_dir" "$role")"
+                if [ -z "$rolepath" ]; then
+                    1>&2 echo ERROR - could not find role "$role" - please use ANSIBLE_COLLECTIONS_PATH
+                    exit 2
+                fi
+                get_repos "$rolepath"
+            done
+        fi
+    done
+}
+
+format_repos_json() {
+    python -c 'import sys; import json; import configparser
+cp = configparser.ConfigParser()
+cp.read_file(sys.stdin)
+ret = []
+# supported is from osbuild-mpp
+supported = ["baseurl", "metalink", "mirrorlist",
+             "enabled", "metadata_expire", "gpgcheck", "username", "password", "priority",
+             "sslverify", "sslcacert", "sslclientkey", "sslclientcert",
+             "skip_if_unavailable"]
+for section in cp.sections():
+    repo = {"id": section}
+    for option in supported:
+        if cp.has_option(section, option):
+            repo[option] = cp.get(section, option)
+    ret.append(repo)
+repo_str = json.dumps(ret)
+repo_str = repo_str.replace("$", "$$$$")
+print(repo_str)
+'
+}
+
+format_repos_raw() {
+    cat
+}
+
+format_repos_yaml() {
+    python -c 'import sys; import yaml; import configparser
+cp = configparser.ConfigParser()
+cp.read_file(sys.stdin)
+ret = []
+for section in cp.sections():
+    repo = {"id": section}
+    for option in cp.options(section):
+        repo[option] = cp.get(section, option)
+    ret.append(repo)
+yaml.safe_dump(ret, sys.stdout)
+'
+}
+
 distro="${distro_ver%%-*}"
+suffix_list=("" "-$distro")
 ver="${distro_ver##*-}"
 if [[ "$ver" =~ ^([0-9]*) ]]; then
     major_ver="${BASH_REMATCH[1]}"
+    suffix_list+=("-${distro}-${major_ver}")
+    if [ "$ver" != "$major_ver" ]; then
+        suffix_list+=("-${distro}-${ver}")
+    fi
 else
     echo ERROR: cannot parse major version number from version "$ver"
     exit 1
